3aa1743ff351d9f2b1a3ddaefe5e03d300cba881
bar2blues <bar2blues@gmail.com>
2019-11-09 18:48:21 -0300

    add defer to script js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*
  *Hacer un request de tipo GET al endpoint https://api.imgflip.com/get_memes, usando Fetch API.
  */
-fetch("https://api.imgflip.com/get_memes", {
-  method: "GET"
+fetch('https://api.imgflip.com/get_memes', {
+  method: 'GET'
 }).then(response => {
   response.status === 200
-    ? console.log("Successful request!")
+    ? console.log('Successful request!')
     : console.error(`Oops, we get an error ${response.status}`);
   return response;
 });
